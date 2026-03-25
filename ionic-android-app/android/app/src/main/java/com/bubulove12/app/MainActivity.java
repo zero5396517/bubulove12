e6ad9d35dd996d5da9fa4dd1dec675e5e2740eeb
mengguanjie <mengguanjie>
2026-03-25 17:42:50 +0800

    ion ic

---
+++ ionic-android-app/android/app/src/main/java/com/bubulove12/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bubulove12.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ ionic-android-app/capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bubulove12.app',
+  appName: 'ionic-android-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
